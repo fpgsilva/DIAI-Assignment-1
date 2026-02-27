5faec3d501586fe46c537015c55c3dcb8f8b5ef4
Francisco <fpg.silva@campus.fct.unl.pt>
2026-02-27 12:56:25 +0000

    Check if works

--- src/main/kotlin/pt/unl/fct/iadi/orderprocessingplatform/domain/OrderItem.kt
+++ src/main/kotlin/pt/unl/fct/iadi/orderprocessingplatform/domain/OrderItem.kt
@@ -1,4 +1,4 @@
 package pt.unl.fct.iadi.orderprocessingplatform.domain
 
-class OrderItem(private val id: String, private val items: List<OrderItem>, private val userId: String, private val createdAt: Instant) {
+class OrderItem(private val id: String, private val items: List<OrderItem>, private val userId: String, private val createdAt: String) {
 }

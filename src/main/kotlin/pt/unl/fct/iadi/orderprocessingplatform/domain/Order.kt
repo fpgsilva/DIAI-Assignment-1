86128409a0afe73ec48f6eb7db029c739f543d94
Francisco <fpg.silva@campus.fct.unl.pt>
2026-03-02 16:40:11 +0000

    Created At try

--- src/main/kotlin/pt/unl/fct/iadi/orderprocessingplatform/domain/Order.kt
+++ src/main/kotlin/pt/unl/fct/iadi/orderprocessingplatform/domain/Order.kt
@@ -7,7 +7,7 @@ class Order        (
         val id: String,
         val items: List<OrderItem>,
         val userId: String,
-        val createdAt: Instant)
+        val createdAt: Instant = Instant.now())
 {
 
     class OrderItem(
